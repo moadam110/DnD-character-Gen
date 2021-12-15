665e80e87f47dc807296d26ab4c912eb3d3a3f04
Ubuntu <azureuser@devjenkins.amsildutfwbe3pjheoftwxi4gf.zx.internal.cloudapp.net>
2021-12-15 15:26:15 +0000

    changes to setup script

--- Jenkins/setup.sh
+++ Jenkins/setup.sh
@@ -6,11 +6,11 @@ echo "Setup stage"
 sudo apt-get update
 sudo apt-get install -y curl jq python3-venv
 
-# # install docker
-# if [ ! -f "/usr/bin/docker" ]; then
-#     curl https://get.docker.com | sudo bash
-#     sudo usermod -aG docker jenkins
-# fi
+# install docker
+if [ ! -f "/usr/bin/docker" ]; then
+    curl https://get.docker.com | sudo bash
+    sudo usermod -aG docker jenkins
+fi
 
 # # install docker compose
  version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
