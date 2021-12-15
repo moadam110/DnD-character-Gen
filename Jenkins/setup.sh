34e560dba29dc2f635e62abd9c6f5edc4afbf803
Ubuntu <azureuser@dev-jenkins.amsildutfwbe3pjheoftwxi4gf.zx.internal.cloudapp.net>
2021-12-15 13:41:22 +0000

    adding dependancy installation stage

--- Jenkins/setup.sh
+++ Jenkins/setup.sh
@@ -3,8 +3,8 @@
 echo "Setup stage"
 
 # # apt dependencies
-# sudo apt-get update
-# sudo apt-get install -y curl jq python3-venv
+sudo apt-get update
+sudo apt-get install -y curl jq python3-venv
 
 # # install docker
 # if [ ! -f "/usr/bin/docker" ]; then
@@ -13,9 +13,9 @@ echo "Setup stage"
 # fi
 
 # # install docker compose
-# version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-# sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+ sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
 
 # docker login
-# docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
+docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
