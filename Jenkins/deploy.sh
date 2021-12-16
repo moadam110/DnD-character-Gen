51cb50953f9357ac6f5f87fff58eada11f03db91
Ubuntu <azureuser@devjenkins.amsildutfwbe3pjheoftwxi4gf.zx.internal.cloudapp.net>
2021-12-16 15:53:04 +0000

    deployment docker3

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deploy stage"
-scp docker-compose.yaml jenksins@swarm-manager:/home/jenkins/docker-compose.yaml
+scp docker-compose.yaml azureuser@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh azureuser@swarm-manager \
 DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
 docker stack deploy --compose-file docker-compose.yaml dnd-app
